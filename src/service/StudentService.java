89569706d2b6fe301702facb9c718bcb0a121271
Priyanshu Shrivastava <priyanshu125590@gmail.com>
2026-02-27 17:42:07 +0530

    Initial commit - Student Management System (JDBC + MySQL)

---
+++ src/service/StudentService.java
@@ -0,0 +1,60 @@
+package service;
+
+import model.Student;
+import util.DBConnection;
+
+import java.sql.*;
+
+public class StudentService {
+
+    public void addStudent(Student student) throws Exception {
+
+        Connection conn = DBConnection.getConnection();
+
+        String sql = "INSERT INTO students (name, email, course) VALUES (?, ?, ?)";
+        PreparedStatement pst = conn.prepareStatement(sql);
+
+        pst.setString(1, student.getName());
+        pst.setString(2, student.getEmail());
+        pst.setString(3, student.getCourse());
+
+        pst.executeUpdate();
+        conn.close();
+
+        System.out.println("Student Added Successfully!");
+    }
+
+    public void viewStudents() throws Exception {
+
+        Connection conn = DBConnection.getConnection();
+
+        String sql = "SELECT * FROM students";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+
+        while (rs.next()) {
+            System.out.println(
+                    rs.getInt("id") + " | " +
+                            rs.getString("name") + " | " +
+                            rs.getString("email") + " | " +
+                            rs.getString("course")
+            );
+        }
+
+        conn.close();
+    }
+
+    public void deleteStudent(int id) throws Exception {
+
+        Connection conn = DBConnection.getConnection();
+
+        String sql = "DELETE FROM students WHERE id=?";
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.setInt(1, id);
+
+        pst.executeUpdate();
+        conn.close();
+
+        System.out.println("Student Deleted Successfully!");
+    }
+}
